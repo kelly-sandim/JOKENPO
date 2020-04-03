7275f04d6dab5775ca5b852afb394178e209e96b
Kelly Sandim <kelly.iwauchi@gmail.com>
2020-04-02 20:41:16 -0400

    Criada função básica para o jokenpo

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,66 @@
+var playerChoice = 0;
+var computerChoice = 0;
+var winner = "";
+
 function play(choice)
 {
-    alert(choice);
+    playerChoice = choice;
+    
+    computerChoice = Math.floor((Math.random() *(3 - 1 + 1)) + 1);
+
+    /*
+    1 - Pedra
+    2 - Papel
+    3 - Tesoura
+     */
+
+     //CASOS PEDRA
+    if((playerChoice == 1) && (computerChoice == 1))
+    {
+        winner = "empate";
+    } 
+
+    else if((playerChoice == 1) && (computerChoice == 2))
+    {
+        winner = "computador";
+    } 
+
+    else if ((playerChoice == 1) && (computerChoice == 3))
+    {
+        winner = "jogador";
+    } 
+    
+    //CASOS PAPEL
+    else if((playerChoice == 2) && (computerChoice == 1))
+    {
+        winner = "jogador";
+    } 
+
+    else if((playerChoice == 2) && (computerChoice == 2))
+    {
+        winner = "empate";
+    } 
+
+    else if ((playerChoice == 2) && (computerChoice == 3))
+    {
+        winner = "computador";
+    }
+
+    //CASOS TESOURA
+    else if((playerChoice == 3) && (computerChoice == 1))
+    {
+        winner = "computador";
+    } 
+
+    else if((playerChoice == 3) && (computerChoice == 2))
+    {
+        winner = "jogador";
+    } 
+
+    else if ((playerChoice == 3) && (computerChoice == 3))
+    {
+        winner = "empate";
+    }
+
+    alert(winner);
 }
